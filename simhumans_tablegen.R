ea85d046ca0823c51de3018fb8eee917587d205a
NDykhuis <NDykhuis@users.noreply.github.com>
2015-04-09 08:50:47 -0700

    Add header and GPL boilerplate to simhumans_tablegen.R

--- simhumans_tablegen.R
+++ simhumans_tablegen.R
@@ -1,3 +1,25 @@
+#
+# simhumans_tablegen.R - given data from human subjects experiment runs,
+#                        compute summary statistics and output to a file
+#                        to allow simulation of these human agents
+# 
+# Copyright (C) 2015  Nathan Dykhuis
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+
+
 library("RSQLite")
 library(ggplot2)
 library(reshape2)
